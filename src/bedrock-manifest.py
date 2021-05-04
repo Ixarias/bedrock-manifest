51513294249c0249fbb34f7d0c0896fce0b92da1
vdiekmanlb <vdiekmanlb@outlook.com>
2021-05-04 16:06:54 -0500

    working no deps

--- src/bedrock-manifest.py
+++ src/bedrock-manifest.py
@@ -1,37 +1,109 @@
 import json
+import os
 from pathlib import Path
-from requests import get
+from uuid import uuid4
+
+behavior_manifest = {
+    "format_version": 2,
+    "header": {
+        "description": "pack.description",
+        "name": "pack.name",
+        "uuid": "",
+        "version": [0, 0, 1],
+        "min_engine_version": [1, 16, 220]
+    },
+    "modules": [
+        {
+            "type": "data",
+            "uuid": "",
+            "version": [0, 0, 1]
+        }
+    ],
+    "dependencies": [
+        {
+            "uuid": "",
+            "version": [0, 0, 1]
+        }
+    ]
+}
+
+resource_manifest = {
+    "format_version": 2,
+    "header": {
+        "description": "pack.description",
+        "name": "pack.name",
+        "uuid": "",
+        "version": [0, 0, 1],
+        "min_engine_version": [1, 16, 200]
+    },
+    "modules": [
+        {
+            "type": "resources",
+            "uuid": "",
+            "version": [0, 0, 1]
+        }
+    ]
+}
+
+languages = [
+    "en_US"
+]
+
+en_US = """
+        pack.name = Example Pack
+        pack.description = Example Pack Description
+        """
 
 def generateManifests():
-    # get uuids
+
     uuid_list = []
-    for item in get("https://www.uuidgenerator.net/api/version4/4").iter_lines():
-        uuid_list.append(item.decode("utf-8"))
+    for i in range(4):
+        uuid_list.append(str(uuid4()))
 
     # resource pack
     pack_path = Path("resource_packs", "TEST_RP")
-    default = json.load(open("rp_manifest.json", "r"))
-    if not pack_path.exists():
-        pack_path.mkdir(parents=True)
+    if not Path(pack_path, "texts").exists():
+        Path(pack_path, "texts").mkdir(parents=True, exist_ok=True)
 
-    default["header"]["uuid"] = uuid_list[0]
-    default["modules"][0]["uuid"] = uuid_list[1]
+    resource_manifest["header"]["uuid"] = uuid_list[0]
+    resource_manifest["modules"][0]["uuid"] = uuid_list[1]
 
-    with Path(pack_path, "manifest.json").open() as f:
-        f.write(json.dumps(default))
+
+    with Path(pack_path, "manifest.json").open(mode="w") as f:
+        f.write(json.dumps(resource_manifest))
+    with Path(pack_path, "texts", "languages.json").open(mode="w") as f:
+        f.write(json.dumps(languages))
+    with Path(pack_path, "texts", "en_US.lang").open(mode="w") as f:
+        f.write(en_US)
+
+    with open("world_resource_packs.json", "w") as rpacks:
+        json.dump([{
+            "pack_id": uuid_list[0],
+            "version": [0, 0, 1]
+        }], rpacks)
 
     # behavior pack
     pack_path = Path("behavior_packs", "TEST_BP")
-    default = json.load(open("bp_manifest.json", "r"))
-    if not pack_path.exists():
-        pack_path.mkdir(parents=True)
+    if not Path(pack_path, "texts").exists():
+        Path(pack_path, "texts").mkdir(parents=True, exist_ok=True)
 
-    default["header"]["uuid"] = uuid_list[2]
-    default["modules"][0]["uuid"] = uuid_list[3]
-    default["dependencies"][0]["uuid"] = uuid_list[0]
+    behavior_manifest["header"]["uuid"] = uuid_list[2]
+    behavior_manifest["modules"][0]["uuid"] = uuid_list[3]
+    behavior_manifest["dependencies"][0]["uuid"] = uuid_list[0]
 
-    with Path(pack_path, "manifest.json").open() as f:
-        f.write(json.dumps(default))
+
+    with Path(pack_path, "manifest.json").open(mode="w") as f:
+        f.write(json.dumps(behavior_manifest))
+    with Path(pack_path, "texts", "languages.json").open(mode="w") as f:
+        f.write(json.dumps(languages))
+    with Path(pack_path, "texts", "en_US.lang").open(mode="w") as f:
+        f.write(en_US)
+
+    with open("world_behavior_packs.json", "w") as bpacks:
+        json.dump([{
+            "pack_id": uuid_list[2],
+            "version": [0, 0, 1]
+        }], bpacks)
 
 if __name__ == "__main__":
     generateManifests()
